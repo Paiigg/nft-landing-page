e8cbdc5f8f007cb1df1e995f1a283c63086eb185
Faiqul Washfi <faiqulwashfi@gmail.com>
2022-09-29 19:58:44 +0700

    revision

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import './App.css';
-import "slick-carousel/slick/slick.css"; 
+import "./App.css";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Trending from './components/Trending';
-import Editor from './components/Editor';
-import Creator from './components/Creator';
-import Community from './components/Community';
-import Footer from './components/Footer';
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Trending from "./components/Trending";
+import Editor from "./components/Editor";
+import Creator from "./components/Creator";
+import Community from "./components/Community";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Hero/>
-      <Trending/>
-      <Editor/>
-      <Creator/>
-      <Community/>
-      <Footer/>
-    
+      <Navbar />
+      <Hero />
+      <Trending />
+      <Editor />
+      <Creator />
+      <Community />
+      <Footer />
     </>
   );
 }
--- src/components/Creator.jsx
+++ src/components/Creator.jsx
@@ -1,156 +1,201 @@
-import React from 'react'
-import { creator1, creator2, creator3, creator4, creator5, creator6, eth } from '../data'
+import React from "react";
+import {
+  creator1,
+  creator2,
+  creator3,
+  creator4,
+  creator5,
+  creator6,
+  eth,
+} from "../data";
 
 const Creator = () => {
   return (
-    <section className='bg-primary'>
-      <div className='max-w-[1240px] mx-auto px-4 py-10 text-white'>
-        <h2 className='text-center font-semibold py-5  text-2xl'>Top Creator</h2>
-        <div className='grid grid-cols-1 md:grid-cols-3'>
-          <div className='bg-secondary rounded-xl m-2 flex items-center justify-between px-4 py-4'>
-            <div className='flex items-center gap-5'>
+    <section className="bg-primary">
+      <div className="max-w-[1240px] mx-auto px-4 py-10 text-white">
+        <h2 className="text-center font-semibold py-5  text-2xl">
+          Top Creator
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="bg-secondary rounded-xl m-2 flex items-center justify-between flex-col md:flex-row px-4 py-4">
+            <div className="flex items-center gap-5">
               <img src={creator1} alt="" />
-              <div className=''>
-                <p className='leading-10'>Lydra</p>
-                <div className='flex items-center gap-3'>
+              <div className="">
+                <p className="leading-10">Lydra</p>
+                <div className="flex items-center gap-3">
                   <img src={eth} alt="" />
-                  <p className='text-green'>191.200 ETH</p>
+                  <p className="text-green">191.200 ETH</p>
                 </div>
-              
               </div>
             </div>
-            <button type='button' className='bg-[#5BA300] rounded-full px-5 py-2'>+ Follow</button>
+            <button
+              type="button"
+              className="bg-[#5BA300] rounded-full px-5 py-2 mt-4 w-full md:w-auto"
+            >
+              + Follow
+            </button>
           </div>
 
-          <div className='bg-secondary rounded-xl m-2 flex items-center justify-between px-4 py-4'>
-            <div className='flex items-center gap-5'>
+          <div className="bg-secondary rounded-xl m-2 flex items-center justify-between flex-col md:flex-row px-4 py-4">
+            <div className="flex items-center  gap-5">
               <img src={creator2} alt="" />
-              <div className=''>
-                <p className='leading-10'>ClipGuy21</p>
-                <div className='flex items-center gap-3'>
+              <div className="">
+                <p className="leading-10">ClipGuy21</p>
+                <div className="flex items-center gap-3">
                   <img src={eth} alt="" />
-                  <p className='text-green'>191.200 ETH</p>
+                  <p className="text-green">191.200 ETH</p>
                 </div>
-              
               </div>
             </div>
-            <button type='button' className='bg-[#5BA300] rounded-full px-5 py-2'>+ Follow</button>
+            <button
+              type="button"
+              className="bg-[#5BA300] rounded-full mt-4 w-full md:w-auto px-5 py-2"
+            >
+              + Follow
+            </button>
           </div>
 
-          <div className='bg-secondary rounded-xl m-2 flex items-center justify-between px-4 py-4'>
-            <div className='flex items-center gap-5'>
+          <div className="bg-secondary rounded-xl m-2 flex items-center justify-between flex-col md:flex-row px-4 py-4">
+            <div className="flex items-center gap-5">
               <img src={creator3} alt="" />
-              <div className=''>
-                <p className='leading-10'>Bejo</p>
-                <div className='flex items-center gap-3'>
+              <div className="">
+                <p className="leading-10">Bejo</p>
+                <div className="flex items-center gap-3">
                   <img src={eth} alt="" />
-                  <p className='text-green'>191.200 ETH</p>
+                  <p className="text-green">191.200 ETH</p>
                 </div>
-              
               </div>
             </div>
-            <button type='button' className='bg-[#5BA300] rounded-full px-5 py-2'>+ Follow</button>
+            <button
+              type="button"
+              className="bg-[#5BA300] rounded-full mt-4 w-full md:w-auto px-5 py-2"
+            >
+              + Follow
+            </button>
           </div>
 
-          <div className='bg-secondary rounded-xl m-2 flex items-center justify-between px-4 py-4'>
-            <div className='flex items-center gap-5'>
+          <div className="bg-secondary rounded-xl m-2 flex items-center justify-between flex-col md:flex-row px-4 py-4">
+            <div className="flex items-center gap-5">
               <img src={creator4} alt="" />
-              <div className=''>
-                <p className='leading-10'>Asep</p>
-                <div className='flex items-center gap-3'>
+              <div className="">
+                <p className="leading-10">Asep</p>
+                <div className="flex items-center gap-3">
                   <img src={eth} alt="" />
-                  <p className='text-green'>191.200 ETH</p>
+                  <p className="text-green">191.200 ETH</p>
                 </div>
-              
               </div>
             </div>
-            <button type='button' className='bg-[#5BA300] rounded-full px-5 py-2'>+ Follow</button>
+            <button
+              type="button"
+              className="bg-[#5BA300] rounded-full w-full md:w-auto mt-4 px-5 py-2"
+            >
+              + Follow
+            </button>
           </div>
 
-          <div className='bg-secondary rounded-xl m-2 flex items-center justify-between px-4 py-4'>
-            <div className='flex items-center gap-5'>
+          <div className="bg-secondary rounded-xl m-2 flex items-center justify-between flex-col md:flex-row px-4 py-4">
+            <div className="flex items-center gap-5">
               <img src={creator5} alt="" />
-              <div className=''>
-                <p className='leading-10'>Siti</p>
-                <div className='flex items-center gap-3'>
+              <div className="">
+                <p className="leading-10">Siti</p>
+                <div className="flex items-center gap-3">
                   <img src={eth} alt="" />
-                  <p className='text-green'>191.200 ETH</p>
+                  <p className="text-green">191.200 ETH</p>
                 </div>
-              
               </div>
             </div>
-            <button type='button' className='bg-[#5BA300] rounded-full px-5 py-2'>+ Follow</button>
+            <button
+              type="button"
+              className="bg-[#5BA300] rounded-full w-full md:w-auto mt-4 px-5 py-2"
+            >
+              + Follow
+            </button>
           </div>
 
-          <div className='bg-secondary rounded-xl m-2 flex items-center justify-between px-4 py-4'>
-            <div className='flex items-center gap-5'>
+          <div className="bg-secondary rounded-xl m-2 flex items-center justify-between px-4 flex-col md:flex-row py-4">
+            <div className="flex items-center gap-5">
               <img src={creator6} alt="" />
-              <div className=''>
-                <p className='leading-10'>Bayu</p>
-                <div className='flex items-center gap-3'>
+              <div className="">
+                <p className="leading-10">Bayu</p>
+                <div className="flex items-center gap-3">
                   <img src={eth} alt="" />
-                  <p className='text-green'>191.200 ETH</p>
+                  <p className="text-green">191.200 ETH</p>
                 </div>
-              
               </div>
             </div>
-            <button type='button' className='bg-[#5BA300] rounded-full px-5 py-2'>+ Follow</button>
+            <button
+              type="button"
+              className="bg-[#5BA300] rounded-full w-full md:w-auto mt-4 px-5 py-2"
+            >
+              + Follow
+            </button>
           </div>
 
-          <div className='bg-secondary rounded-xl m-2 flex items-center justify-between px-4 py-4'>
-            <div className='flex items-center gap-5'>
+          <div className="bg-secondary rounded-xl m-2 flex items-center justify-between flex-col md:flex-row px-4 py-4">
+            <div className="flex items-center gap-5">
               <img src={creator1} alt="" />
-              <div className=''>
-                <p className='leading-10'>Lydra</p>
-                <div className='flex items-center gap-3'>
+              <div className="">
+                <p className="leading-10">Lydra</p>
+                <div className="flex items-center gap-3">
                   <img src={eth} alt="" />
-                  <p className='text-green'>191.200 ETH</p>
+                  <p className="text-green">191.200 ETH</p>
                 </div>
-              
               </div>
             </div>
-            <button type='button' className='bg-[#5BA300] rounded-full px-5 py-2'>+ Follow</button>
+            <button
+              type="button"
+              className="bg-[#5BA300] rounded-full w-full md:w-auto mt-4 px-5 py-2"
+            >
+              + Follow
+            </button>
           </div>
 
-          <div className='bg-secondary rounded-xl m-2 flex items-center justify-between px-4 py-4'>
-            <div className='flex items-center gap-5'>
+          <div className="bg-secondary rounded-xl m-2 flex items-center justify-between flex-col md:flex-row px-4 py-4">
+            <div className="flex items-center gap-5">
               <img src={creator1} alt="" />
-              <div className=''>
-                <p className='leading-10'>Lydra</p>
-                <div className='flex items-center gap-3'>
+              <div className="">
+                <p className="leading-10">Lydra</p>
+                <div className="flex items-center gap-3">
                   <img src={eth} alt="" />
-                  <p className='text-green'>191.200 ETH</p>
+                  <p className="text-green">191.200 ETH</p>
                 </div>
-              
               </div>
             </div>
-            <button type='button' className='bg-[#5BA300] rounded-full px-5 py-2'>+ Follow</button>
+            <button
+              type="button"
+              className="bg-[#5BA300] rounded-full w-full md:w-auto mt-4 px-5 py-2"
+            >
+              + Follow
+            </button>
           </div>
 
-          <div className='bg-secondary rounded-xl m-2 flex items-center justify-between px-4 py-4'>
-            <div className='flex items-center gap-5'>
+          <div className="bg-secondary rounded-xl m-2 flex items-center justify-between flex-col md:flex-row px-4 py-4">
+            <div className="flex items-center gap-5">
               <img src={creator1} alt="" />
-              <div className=''>
-                <p className='leading-10'>Lydra</p>
-                <div className='flex items-center gap-3'>
+              <div className="">
+                <p className="leading-10">Lydra</p>
+                <div className="flex items-center gap-3">
                   <img src={eth} alt="" />
-                  <p className='text-green'>191.200 ETH</p>
+                  <p className="text-green">191.200 ETH</p>
                 </div>
-              
               </div>
             </div>
-            <button type='button' className='bg-[#5BA300] rounded-full px-5 py-2'>+ Follow</button>
+            <button
+              type="button"
+              className="bg-[#5BA300] rounded-full mt-4 w-full md:w-auto px-5 py-2"
+            >
+              + Follow
+            </button>
           </div>
-
-
-
         </div>
-        <div className='flex justify-center items-center py-4'>
-          <button className='border border-green rounded-xl text-green px-6 py-3'>View Rangking</button>
+        <div className="flex justify-center items-center py-4">
+          <button className="border border-green rounded-xl text-green px-6 py-3">
+            View Rangking
+          </button>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Creator
+export default Creator;
